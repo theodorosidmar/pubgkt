eaaf77ec63b36e372121269ac33a0f4d5ff62356
sid <theodorosidmar@gmail.com>
2026-03-31 01:09:03 -0300

    chore: Sets apiVersion & languageVersion to kmp convention

--- buildSrc/src/main/kotlin/kmp-library.gradle.kts
+++ buildSrc/src/main/kotlin/kmp-library.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_3
 
 plugins {
     kotlin("multiplatform")
@@ -8,6 +9,8 @@ kotlin {
     compilerOptions {
         allWarningsAsErrors = true
         progressiveMode = true
+        apiVersion = KOTLIN_2_3
+        languageVersion = KOTLIN_2_3
     }
 
     jvm {
