07980e8ed169ad287fa770f713845a3db0a3252b
sid <theodorosidmar@gmail.com>
2026-04-08 22:41:07 -0300

    chore(common): Creates PubgktInternal annotation class

---
+++ common/src/commonMain/kotlin/pubgkt/Annotations.kt
@@ -0,0 +1,17 @@
+package pubgkt
+
+import kotlin.RequiresOptIn.Level.ERROR
+import kotlin.annotation.AnnotationRetention.BINARY
+import kotlin.annotation.AnnotationTarget.*
+
+/**
+ * Marks an API for internal use only.
+ */
+@MustBeDocumented
+@Retention(BINARY)
+@Target(CLASS, PROPERTY, FUNCTION, TYPEALIAS)
+@RequiresOptIn(
+    message = "This API is intended for internal use only.",
+    level = ERROR,
+)
+public annotation class PubgktInternal
