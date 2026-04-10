f96398506e62f6b2a964b1e0e3042a24c8bb4022
sid <theodorosidmar@gmail.com>
2026-04-10 12:15:46 -0300

    chore(buildsrc): Creates Library object

---
+++ buildSrc/src/main/kotlin/Library.kt
@@ -0,0 +1,7 @@
+internal object Library {
+    const val GROUP       = "pubgkt"
+    const val VERSION     = "0.1.0-SNAPSHOT"
+    const val NAME        = "pubgkt"
+    const val DESCRIPTION = "Kotlin Multiplatform PUBG API client"
+    const val PROJECT_URL = "https://github.com/theodorosidmar/pubgkt"
+}
