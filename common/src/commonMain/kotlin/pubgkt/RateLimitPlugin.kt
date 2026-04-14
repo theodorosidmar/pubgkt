0aa8ef18f63b0fcb6d54078df7b1a24f60b304b2
sid <theodorosidmar@gmail.com>
2026-04-14 00:08:36 -0300

    refactor(common): Renames RateLimitPlugin name

--- common/src/commonMain/kotlin/pubgkt/RateLimitPlugin.kt
+++ common/src/commonMain/kotlin/pubgkt/RateLimitPlugin.kt
@@ -2,7 +2,7 @@ package pubgkt
 
 import io.ktor.client.plugins.api.createClientPlugin
 
-internal val RateLimitPlugin = createClientPlugin("PubgktRateLimit", ::RateLimitConfig) {
+internal val RateLimitPlugin = createClientPlugin("PubgRateLimit", ::RateLimitConfig) {
     val limiter = pluginConfig.rateLimiter
 
     onRequest { _, _ ->
