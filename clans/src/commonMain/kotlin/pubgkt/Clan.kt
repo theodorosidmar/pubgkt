6992ca70f6ad7f70011c3d40962bd98ab755580e
sid <theodorosidmar@gmail.com>
2026-04-16 23:20:07 -0300

    chore(clans): Adds KDoc to Clan data class

--- clans/src/commonMain/kotlin/pubgkt/Clan.kt
+++ clans/src/commonMain/kotlin/pubgkt/Clan.kt
@@ -1,9 +1,28 @@
 package pubgkt
 
 public data class Clan(
+    /**
+     * Clan id
+     */
     val id: String,
+
+    /**
+     * Clan name (e.g: SPARKINGG)
+     */
     val name: String,
+
+    /**
+     * Clan tag (e.g: KING)
+     */
     val tag: String,
+
+    /**
+     * Clan level (e.g: 20)
+     */
     val level: Int,
+
+    /**
+     * Clan member count (e.g: 100)
+     */
     val memberCount: Int,
 )
