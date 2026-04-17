0db6cec39d563f2814a84f84a83309d419ec2a9c
sid <theodorosidmar@gmail.com>
2026-04-16 23:10:25 -0300

    chore(common): Adds Map

---
+++ common/src/commonMain/kotlin/pubgkt/Map.kt
@@ -0,0 +1,12 @@
+package pubgkt
+
+public enum class Map {
+    BALTIC_MAIN,
+    DESERT_MAIN,
+    DIHOROTOK_MAIN,
+    ERANGEL_MAIN,
+    RANGE_MAIN,
+    SAVAGE_MAIN,
+    SUMMERLAND_MAIN,
+    TIGER_MAIN,
+}
