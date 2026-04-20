c825f2c87bc5334768d9d9adabab1dea266615aa
sid <theodorosidmar@gmail.com>
2026-04-20 19:50:47 -0300

    docs(common): Adds KDoc to HttpClient.get

--- common/src/commonMain/kotlin/pubgkt/HttpClientExtensions.kt
+++ common/src/commonMain/kotlin/pubgkt/HttpClientExtensions.kt
@@ -4,6 +4,13 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 
+/**
+  * Performs an HTTP GET request to the given [urlString] with the specified [policy].
+  *
+  * @param urlString The URL to send the GET request to.
+  * @param policy The [RequestPolicy] to apply to this request. Defaults to [DefaultRequestPolicy].
+  * @return The [HttpResponse] received from the server.
+  */
 @PubgktInternal
 public suspend fun HttpClient.get(
     urlString: String,
